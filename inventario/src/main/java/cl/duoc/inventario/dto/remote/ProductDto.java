6667d8b1e616e82ee1138e44601617a1945123d0
isidora <is.gomezj@duocuc.cl>
2026-05-22 20:03:51 -0400

    Creación de DTOs con respectiva separación en paquetes request y response, ademas de remote.

---
+++ inventario/src/main/java/cl/duoc/inventario/dto/remote/ProductDto.java
@@ -0,0 +1,21 @@
+package cl.duoc.inventario.dto.remote;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDto {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Integer price;
+
+    private CategoryDto category;
+
+}
